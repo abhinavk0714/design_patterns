6cfa2b15dbd4754242458d51b51b7058ac686c88
Abhinav Krishniah <abhinavk@Abhinavs-MacBook-Air-2.local>
2024-02-07 16:34:14 -0500

    done javadoc

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,23 +1,38 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * FlashCards class to create a set of flashcards to study
+ * @author abhinavk
+ */ 
 public class FlashCards {
     private Random rand;
     private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
 
+    /**
+     * Private constructor that allows for no multiple instances
+     */
     private FlashCards() {
         rand = new Random();
         words = FileReader.getWords();
     }
 
+    /**
+     * Static method to ensure that there is only one instance of FlashCards
+     * @return the flashCards set instance
+     */
     public static FlashCards getInstance() {
         if(flashCards == null)
             flashCards = new FlashCards();
         return flashCards;    
     }
 
+    /**
+     * Gets a random word from the list of flashcards
+     * @return the current word
+     */
     public Word getWord() {
         if(words.isEmpty())
             return null;
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,10 +1,20 @@
-
+/**
+ * Word class that gives the desciption of the words in FlashCards
+ * @author abhinavk
+ */
 public class Word {
     private String word;
     private String type;
     private String definition;
     private String sentence;
 
+    /**
+     * Constructor that initializes the word, type, definition, and sentence
+     * @param word the word
+     * @param type the word's type
+     * @param definition the word's definition
+     * @param sentence the word in a sentence
+     */
     public Word(String word, String type, String definition, String sentence) {
         this.word = word;
         this.type = type;
@@ -12,10 +22,18 @@ public class Word {
         this.sentence = sentence;
     }
 
+    /**
+     * Method that gets a word
+     * @return a word
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     * Method that returns a string representation of the description of the word
+     * @return the description of the word
+     */
     public String getDescription() {
         return "Part of Speech: " + type + "\nDefinition: " + definition + "\n" + sentence;
     }
